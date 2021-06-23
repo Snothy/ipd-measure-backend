ef060958b28e9abab68d15969f7e71d73afff264
drumevp <drumevp@coventry.ac.uk>
2021-06-24 02:40:13 +0300

    Receive & parse image from frontend
    Very simple task, but tedious due to poor documentation.

--- routes/methodOne.js
+++ routes/methodOne.js
@@ -1,17 +1,25 @@
 const Router = require('koa-router');
-const bodyparser = require('koa-bodyparser');
-//const model = require('../models/mothodOne');
+
+const koaBody = require('koa-body')({multipart: true, uploadDir: './uploads'});
+
+
 
 const router = Router({ prefix: '/api/methodOne' });
 
 router.get('/', getAll);
-router.post('/', processImages);
+router.post('/', koaBody, processImages);
 
 async function getAll(ctx) {
     return ctx.body = {msg: "hi"};
 }
 
 async function processImages(ctx) {
+    console.log(ctx.request);
+    //console.log(Object.keys(ctx.request.body));
+    console.log(ctx.request.files);
+    //const photo = ctx.request.body.photo;
+    //console.log(photo);
+    
     return ctx.body = {msg: "bye"};
 }
 
